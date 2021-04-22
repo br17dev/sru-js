cd483202c2bfbf747a8ceb020b37eec13fc7d39c
Ben R <ben.dojrp@gmail.com>
2021-04-22 17:22:39 +0100

    update

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ const config = require("./config.json");
 
 const client = new Discord.Client();
 
-const prefix = "-";
+const prefix = config.prefix;
+
+client.on("ready", function(){
+
+	console.log(`Logged in: ${client.user.tag}`);
+  client.user.setActivity(`${config.prefix}help`);
+
+});
+
+client.on("guildMemberAdd", (member) => {
+	console.log(member)
+	const channel = client.channels.cache.find(channel => channel.name === "welcome")
+	const welcomeeb = new Discord.MessageEmbed()
+	.setColor(`#000000`)
+	.setTitle(`New Member Alert`)
+	.setThumbnail(`https://imgur.com/Wgggksj.png`)
+	.setDescription(`${member.user.username} (<@!${member.id}>) has joined the DoJRP SRU Discord and is awaiting verification and permissions.`)
+	.setFooter(`San Andreas Strategic Response Unit 2021`)
+	.setTimestamp()
+	channel.send(welcomeeb)
+});
 
 client.on("message", function(message) {
   if (message.author.bot) return;
@@ -13,7 +33,8 @@ client.on("message", function(message) {
   const args = commandBody.split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  // Training Announcement Command
+  // ALL COMMANDS AVAILABLE
+
   if (command === "train") {
     message.delete({timeout: 100})
     .then(msg => console.log(`Deleted message from ${msg.author.username} after 10 miliseconds`))
@@ -142,7 +163,46 @@ client.on("message", function(message) {
 
     		channel.send("<@&>")
         channel.send(eodeb)
-      } else {
+      }
+			else if (args[0] === `ingame`) {
+				const ingameeb = new Discord.MessageEmbed()
+				.setColor(`#000000`)
+				.setTitle(`New Basic In Game Training`)
+				.setThumbnail(`https://imgur.com/Wgggksj.png`)
+				.setDescription(`There will be a **SRU Basic In Game** training on **${args[1]}** at **${args[2]}**.\n\nThis training will be hosted by ${message.author}.\n`)
+				.addFields(
+					{ name: `Requirements:`, value: `- Must be an SRU Recruit within the San Andreas Strategic Response Unit\n- Must be able to attend for the whole duration of this training.\n- Must have completed and passed the Basic In Class Training.`},
+					{ name: `Assistants:`, value: `Any SRU Officer I+ who wishes to assist, please react with <:SRUSpecialist:833487986787483648> and comment on the event link below.`},
+					{ name: `Event Link:`, value: `${args[3]}`},
+				)
+				.setTimestamp()
+				.setFooter(`San Andreas Strategic Response Unit 2021`);
+
+				channel.send("<@&>")
+				channel.send(ingameeb)
+
+			}
+
+			else if (args[0] === `inclass`) {
+				const inclasseb = new Discord.MessageEmbed()
+				.setColor(`#000000`)
+				.setTitle(`New Basic In Class Training`)
+				.setThumbnail(`https://imgur.com/Wgggksj.png`)
+				.setDescription(`There will be a **SRU Basic In Class** training on **${args[1]}** at **${args[2]}**.\n\nThis training will be hosted by ${message.author}.\n`)
+				.addFields(
+					{ name: `Requirements:`, value: `- Must be an SRU Recruit within the San Andreas Strategic Response Unit\n- Must be able to attend for the whole duration of this training.\n- Must have an accepted SRU Application and Interview.`},
+					{ name: `Assistants:`, value: `Any SRU Specialist+ who wishes to assist, please react with <:SRUSpecialist:833487986787483648> and comment on the event link below.`},
+					{ name: `Event Link:`, value: `${args[3]}`},
+				)
+				.setTimestamp()
+				.setFooter(`San Andreas Strategic Response Unit 2021`);
+
+				channel.send("<@&>")
+				channel.send(inclasseb)
+
+			}
+
+			else {
         message.reply(`It does not appear this training type exists (${args[0]})! Please make sure to choose a correct training type.`)
       }
 
@@ -152,7 +212,6 @@ client.on("message", function(message) {
       }
     }
 
-//DONE
   else if (command === "documents") {
     message.delete({timeout: 100})
     .then(msg => console.log(`Deleted message from ${msg.author.username} after 10 miliseconds`))
@@ -222,6 +281,37 @@ client.on("message", function(message) {
     }
   }
 
+  else if (command === "callout"){
+    if(message.member.roles.cache.some(r => r.name === "SRU Supervisor") || message.member.roles.cache.some(r => r.name === "SRU Commander") || message.member.roles.cache.some(r => r.name === "SRU Deputy Director") || message.member.roles.cache.some(r => r.name === "SRU Director")) {
+        message.delete({timeout: 100})
+        .then(msg => console.log(`Deleted message from ${msg.author.username} after 10 miliseconds`))
+        .catch(console.error);
+        const channel = client.channels.cache.find(channel => channel.name === "sru-callout")
+        channel.send("**__Welcome to the SRU Callout Chat__**\n\nThink of this channel as your pager for SRU deployment notifications. Any @SRU Tac Response can notify of an active situation and request additional units to assist in server. If you have sufficient units in server already, there is no need to request them here. There must be at least 3 spots open in your server to make a callout request here!\n\n**Instructions:**\n```SRU Officers requesting deployment of additional SRU Officers to their server can use the following command:\n-deploy [Server #] [Brief Description of Call (Barricaded Person, Hostage Situation, etc.)]\n\nSRU Officers wishing to clear a previous request once the situation is Code 4 should use the below command\n\n-clear [Callout ID]```\nWe hope this helps provide better response times and numbers for responses. Anyone caught abusing the features of this system will lose access to it for a temporary or indefinite time depending on the circumstances. If you have any issues or questions regarding this bot, see Ben R. 5S-34.")
+      } else {
+        return message.reply(`You must be an SRU Supervisor or above in order to use this command. If you think this is a mistake, please contact a member of the SRU Supervisory Team.`)
+      }
+    }
+
+  else if (command === "info"){
+    message.delete({timeout: 100})
+    .then(msg => console.log(`Deleted message from ${msg.author.username} after 10 miliseconds`))
+    .catch(console.error);
+    const infoeb = new Discord.MessageEmbed()
+    .setColor(`#000000`)
+    .setTitle(`SRU Discord Bot Information`)
+    .setThumbnail(`https://imgur.com/Wgggksj.png`)
+    .setDescription(``)
+    .addFields(
+      {name:`Version:`, value:`${config.bot_version}`,inline: true},
+      {name:`Developers:`, value:`<@!427518474139467796>`,inline:true},
+      {name:`Ping:`, value:`${Date.now() - message.createdTimestamp}ms`,inline:true},
+    )
+    .setTimestamp()
+    .setFooter(`San Andreas Strategic Response Unit 2021`)
+    message.channel.send(infoeb)
+
+  }
 });
 
-client.login(config.BOT_TOKEN);
+client.login(config.token);
