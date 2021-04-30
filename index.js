17d78fc4207e9e9feb46a66c9274d273c70531af
Ben R <ben.dojrp@gmail.com>
2021-04-30 15:40:52 +0100

    c

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.on("guildMemberAdd", (member) => {
 	.setColor(`#00ff00`)
 	.setTitle(`New Member Alert`)
 	.setThumbnail(`https://imgur.com/Wgggksj.png`)
-	.setDescription(`<@${member.id}> (${member.user.username}#${member.user.discriminator}) has joined the DoJRP SRU Discord and is awaiting verification and permissions.`)
+	.setDescription(`<@!${member.id}> (${member.user.username}#${member.user.discriminator}) has joined the DoJRP SRU Discord and is awaiting verification and permissions.`)
 	.setFooter(`San Andreas Strategic Response Unit 2021`)
 	.setTimestamp()
 	channel.send(welcomeeb)
