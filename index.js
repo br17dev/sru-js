3ec0b7903f9302964a31f4d3f3bc3b7bd385102d
Ben R <ben.dojrp@gmail.com>
2021-04-30 16:21:01 +0100

    g.a

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ client.on("guildMemberAdd", (member) => {
 	.setDescription(`<@!${member.id}> (${member.user.username}#${member.user.discriminator}) has joined the DoJRP SRU Discord and is awaiting verification and permissions.`)
 	.setFooter(`San Andreas Strategic Response Unit 2021`)
 	.setTimestamp()
-	channel.send('This is a normal message.', {
- embed: welcomeeb,
-});
+	channel.send(welcomeeb)
 });
 
 client.on("message", function(message) {
